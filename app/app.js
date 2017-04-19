870e1e722cfad18cc1c658559d19c94b11957f1b
rossar19 <ari_ross19@yahoo.com>
2017-04-18 22:04:42 -0400

    create connection to db

--- app/app.js
+++ app/app.js
@@ -4,4 +4,17 @@ module.exports = function(app) {
     require ("./services/user.service.server.js")(app, model);
     // require ("./services/plan.service.server.js")(app, model);
     require ("./services/trip.service.server.js")(app, model);
+
+    var connectionString = 'mongodb://127.0.0.1:27017/globetrotter';
+
+    if(process.env.MLAB_USERNAME) {
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
+    }
+
+    var mongoose = require("mongoose");
+    mongoose.createConnection(connectionString);
 };
