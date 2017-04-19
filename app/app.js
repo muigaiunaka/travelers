8f7e5e61e542d9811d55dbbf07283fbe886bcbf3
rossar19 <ari_ross19@yahoo.com>
2017-04-18 23:02:26 -0400

    remove comments

--- app/app.js
+++ app/app.js
@@ -1,10 +1,8 @@
 module.exports = function(app) {
 
     var connectionString = 'mongodb://127.0.0.1:27017/globetrotter';
-    console.log(process.env.MLAB_USERNAME);
 
     if(process.env.MLAB_USERNAME) {
-    	console.log('go');
         connectionString = process.env.MLAB_USERNAME + ":" +
             process.env.MLAB_PASSWORD + "@" +
             process.env.MLAB_HOST + ':' +
